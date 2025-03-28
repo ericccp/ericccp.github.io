a48869bc70b598e2f3c9ce120f3b9c09a98a0410
ericccp <ericccp@users.noreply.github.com>
2025-03-28 17:31:06 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import spectre from './package/src';
-
-import node from '@astrojs/node';
-import { spectreDark } from './src/ec-theme';
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,40 +11,6 @@ export default defineConfig({
   },
   output: 'static',
   integrations: [
-    expressiveCode({
-      themes: [spectreDark],
-    }),
-    mdx(),
-    sitemap(),
-    spectre({
-      name: 'Spectre',
-      openGraph: {
-        home: {
-          title: 'Spectre',
-          description: 'A minimalistic theme for Astro.'
-        },
-        blog: {
-          title: 'Blog',
-          description: 'News and guides for Spectre.'
-        },
-        projects: {
-          title: 'Projects'
-        }
-      },
-      giscus: {
-        repository: 'louisescher/spectre',
-        repositoryId: 'R_kgDONjm3ig',
-        category: 'General',
-        categoryId: 'DIC_kwDONjm3is4ClmBF',
-        mapping: 'pathname',
-        strict: true,
-        reactionsEnabled: true,
-        emitMetadata: false,
-        lang: 'en',
-      }
-    })
-  ],
-  adapter: node({
-    mode: 'standalone'
-  })
+    mdx()
+  ]
 });
